094fdac654d9173d5db96443c936f8d1c7198d1a
SomnathSinha1 <somnaths1996@gmail.com>
2016-09-29 21:39:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,13 @@
 console.log('Loaded!');
 var img= document.getElementById('img');
 var marginleft=0;
+function moveRight()
+{
+    marginleft = marginleft + 10;
+    img.style.marginleft = marginleft + 'px';
+}
 img.onclick =  function()
 {
-       //var interval=setInterval(moveRight, 100);
-        img.style.marginleft = '100px';
+       var interval=setInterval(moveRight, 100);
+        //img.style.marginleft = '100px';
 };
