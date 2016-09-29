30c81134f5a1023646a977de5f0ef943cb506efc
SomnathSinha1 <somnaths1996@gmail.com>
2016-09-29 21:29:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,9 +89,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
