fe6127f5e68a7797d4b6666cfce918a07a121556
SomnathSinha1 <somnaths1996@gmail.com>
2016-09-30 21:38:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/ui/main.js', function (req, res) {
 var counter=0;
 app.get('/counter', function (req, res) {
   res.send(counter.toString());
-  };
+});
 app.get('/:articleName', function (req, res){
   var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
