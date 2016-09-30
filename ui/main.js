95288427c420261275eb9cd67c88a1edd8e8edc5
SomnathSinha1 <somnaths1996@gmail.com>
2016-09-30 21:49:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,7 @@
-console.log('Loaded!');
-var img = document.getElementById('madi');
-var marginleft=0;
-function moveRight()
+var button=document.getElementById('counter');
+button.onclick= function()
 {
-    marginleft = marginleft + 1;
-    img.style.marginleft = marginleft + 'px';
-}
-img.onclick =  function()
-{
-       var interval=setInterval(moveRight, 10);
-       //img.style.marginleft = '100px';
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML= counter.toString();
 };
