f52fafc8664fdf96de4bac15f37fdde3c1495f4b
SomnathSinha1 <somnaths1996@gmail.com>
2016-10-10 12:14:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,9 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/favicon.ico', function (req, res) {
+  res.send('null');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
