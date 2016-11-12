120cb885f17822feac7eabb6f68b137075519c6a
Somnath Sinha <somnaths1996@gmail.com>
2016-11-12 12:29:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ return htmltemplate;
 }
 function hash(input, salt){
     var hashed =crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return ['pbkdf2Sync', '10000' , salt, hashed.toString('hex')].join('$');
 }
    
 app.get('/hash/:input', function (req, res) {
